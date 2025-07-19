7e633f85f8efbda3b6bf304b023c4d2d3dccd94b
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-19 22:45:29 +0100

    Implement demo

--- demo.go
+++ demo.go
@@ -1,1 +1,35 @@
 package boredemo
+
+import (
+	"context"
+
+	"go.trulyao.dev/bore/v2/clipboard"
+)
+
+var clipboardInstance clipboard.NativeClipboard
+
+func init() {
+	cp, err := clipboard.NewNativeClipboard()
+	if err != nil {
+		panic("failed to initialize clipboard: " + err.Error())
+	}
+
+	clipboardInstance = cp
+}
+
+func ReadClipboard() (string, error) {
+	ctx := context.Background()
+
+	b, err := clipboardInstance.Read(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
+
+func WriteClipboard(text string) error {
+	ctx := context.Background()
+	err := clipboardInstance.Write(ctx, []byte(text))
+	return err
+}
